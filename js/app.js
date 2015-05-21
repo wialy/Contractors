54f867de133abfe6c62e51d8e40de521ba472033
Alex Ilczenko <a.ilczenko@heuwell.pl>
2015-05-21 10:56:11 +0200

    Moved resize to service

--- js/app.js
+++ js/app.js
@@ -2,57 +2,84 @@
 
 angular.module('contractorsApp', [])
 
-.controller('BodyController', ['$scope', '$document', '$window', function($scope, $document, $window){
+.service('DataService', ['$rootScope', function($rootScope){
+	var service = {
+		contractors : [],
+		addContractor : function (args) {
+			service.contractos.push(args);
+		},
+	};
+	return service;
+}])
 
+.service('ResizeService', ['$rootScope', '$document', '$window', function($rootScope, $document, $window) {
+	var service = {
+		w:	0,
+		h:	0
+	};
+	
+	var onResize = function() {
+		service.w = $document[0].body.clientWidth;
+		service.h = $window.innerHeight;
+		
+		$rootScope.$broadcast('event.resize', {
+			w:	service.w,
+			h:	service.h 
+		});
+	};
+	
+	angular.element($window).bind('resize', onResize);
+	onResize();
+	
+	return service;
+}])
+
+.controller('BodyController', ['$scope', 'ResizeService', function($scope, ResizeService){
 	var self = this;
-
+	
 	this.menuStyle = {};
 	this.contaierStyle = {};
 	this.isShort = false;
+	
+	this.onResize = function(){
+		var w = ResizeService.w;
+		var h = ResizeService.h;
 
-	//socket.connect('https://sand.geocom.pro:8056', { query: 'sessionID=sesja1' });
-
-	this.onResize = function() {
-
-			var w = self.windowWidth = 	$document[0].body.clientWidth;
-			var h = self.windowHeight = $window.innerHeight;
-
-			if(w < h || (w > h && h > 480)) {
-				// top
-				self.menuStyle = {
-					width: 	w + 'px',
-					height: '30px',
-					float: 'none'
-				};
-				self.mainStyle = {
-					width: 	w + 'px',
-					height: (h-30) + 'px',
-					float: 'none'
-				};
-				self.isShort = w < 400;
-			} else {
-				// side
-				self.menuStyle = {
-					width: 	'30px',
-					height: h + 'px',
-					float: 'left'
-				};
-				self.mainStyle = {
-					width: 	(w-30) + 'px',
-					height: h + 'px',
-					float: 'left'
-				};
-				self.isShort = true;
-			}
-
-			if(!$scope.$$phase) {
-				$scope.$apply();
-			}
-	}
-
-	angular.element($window).bind('resize', this.onResize);
+		if(w < h || (w > h && h > 480)) {
+			// top
+			self.menuStyle = {
+				width: 	w + 'px',
+				height: '30px',
+				float: 'none'
+			};
+			self.mainStyle = {
+				width: 	w + 'px',
+				height: (h-30) + 'px',
+				float: 'none'
+			};
+			self.isShort = w < 400;
+		} else {
+			// side
+			self.menuStyle = {
+				width: 	'30px',
+				height: h + 'px',
+				float: 'left'
+			};
+			self.mainStyle = {
+				width: 	(w-30) + 'px',
+				height: h + 'px',
+				float: 'left'
+			};
+			self.isShort = true;
+		}
+		
+		if(!$scope.$$phase) {
+			$scope.$apply();
+		}	
+	};
+	
+	$scope.$on('event.resize', this.onResize); 
 	this.onResize();
-
 }]);
 
 })();
