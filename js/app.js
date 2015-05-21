f1688d5bc7b506217cd394463e9a50b5600f5fba
Alex Ilczenko <a.ilczenko@heuwell.pl>
2015-05-21 12:15:35 +0200

    Added DataService

--- js/app.js
+++ js/app.js
@@ -3,12 +3,71 @@
 angular.module('contractorsApp', [])
 
 .service('DataService', ['$rootScope', function($rootScope){
+	
+	var CMD_LOGIN = 		'login',
+		CMD_GET_LIST = 		'CRM.query.getOrganizationList';
+	
 	var service = {
-		contractors : [],
+		isConnected:	false,
+		isLoggedIn:		false,
+		contractors : 	[],
 		addContractor : function (args) {
 			service.contractos.push(args);
 		},
+		login: function(login, password){
+			sendCommand(CMD_LOGIN, { login:login, password:password });	
+		},
+		getList:	function(){
+			sendCommand(CMD_GET_LIST);
+		}
 	};
+	
+	var socket = io.connect('https://sand.geocom.pro:8056', { query: 'sessionID=sesja1' });
+	var onConnected = function() {
+		service.isConnected = true;
+		$rootScope.$broadcast('event.connected');
+	};
+	socket.on('connect', onConnected);
+	
+	var commandID = 0;
+	var pendingCommands = {};
+	
+	function sendCommand(method, params) {
+		pendingCommands[commandID] = {
+			method:	method,
+			params:	params
+		};
+		
+		socket.emit('command', {
+			jsonrpc:	'2.0',
+			id:			String(commandID++),
+			method:		method,
+			params:		params
+	    });
+	}	
+	
+	socket.on('commandResult', function (params) {
+		console.log('commandResult', params);
+		
+		var pendingCommand = pendingCommands[params.id];
+		
+		if(pendingCommand !== null && pendingCommand !== undefined) {
+			switch(pendingCommand.method) {
+				case CMD_LOGIN:
+					service.isLogged = params.result;
+					$rootScope.$broadcast('event.login', service.isLogged);
+					// console.log('isLogged', service.isLogged);
+				break;
+				
+				case CMD_GET_LIST:
+					service.contractors = params.result;
+					$rootScope.$broadcast('event.update', service.contractors);
+				break;	
+			}
+			delete pendingCommands[params.id];
+		}
+	});
+		
 	return service;
 }])
 
@@ -34,14 +93,21 @@ angular.module('contractorsApp', [])
 	return service;
 }])
 
-.controller('BodyController', ['$scope', 'ResizeService', function($scope, ResizeService){
+.controller('BodyController', ['$scope', 'DataService', 'ResizeService', function($scope, DataService, ResizeService){
 	var self = this;
 	
 	this.menuStyle = {};
 	this.contaierStyle = {};
 	this.isShort = false;
+	this.contractors = [];
 	
-	this.onResize = function(){
+	this.updateScope = function() {
+		if(!$scope.$$phase) {
+			$scope.$apply();
+		}	
+	};
+	
+	this.onResize = function(e, args){
 		var w = ResizeService.w;
 		var h = ResizeService.h;
 
@@ -73,10 +139,25 @@ angular.module('contractorsApp', [])
 			self.isShort = true;
 		}
 		
-		if(!$scope.$$phase) {
-			$scope.$apply();
-		}	
+		self.updateScope();
 	};
+	
+	$scope.$on('event.connected', function(){
+		DataService.login('root', '12345');
+	});
+	
+	$scope.$on('event.login', function(e, args){
+		if(args === true) {
+			DataService.getList();
+		}
+	});
+	
+	
+	$scope.$on('event.update', function(e, args){
+		self.contractors = args;
+		self.updateScope();
+		// console.log(self.contractors);
+	});
 	
 	$scope.$on('event.resize', this.onResize); 
 	this.onResize();
